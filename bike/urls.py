76b8e233b239de3800b00da482ae32fb04909ec4
Lucas Souza <lucas.souza@leoo.com.br>
2022-08-25 16:50:41 -0300

    fix url bike

--- bike/urls.py
+++ bike/urls.py
@@ -31,7 +31,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('swagger/', schema_view.with_ui('swagger'), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger'), name='schema-swagger-ui'),
     path('reservation/', include('reservation.urls')),
     path('accessToken/', TokenObtainPairView.as_view()),
     path('accessToken/refresh/', TokenRefreshView.as_view()),

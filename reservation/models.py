3cc4e5dde4c7caca21659534e58f701239b86640
Lucas Souza <lucas.souza@leoo.com.br>
2022-08-31 12:32:16 -0300

    fix models cpf and deploy

--- reservation/models.py
+++ reservation/models.py
@@ -9,7 +9,7 @@ class Reservation(models.Model):
     id = models.UUIDField(primary_key=True,default=uuid.uuid4,null=False,blank=False,editable=False) # O editable=False faz com que o id nao fique visivel para edicao
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    cpf = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=255,default='string')
     bike_model = models.TextField()
     number_allocation = models.IntegerField()
     date_entry = models.DateField(default=date.today)
